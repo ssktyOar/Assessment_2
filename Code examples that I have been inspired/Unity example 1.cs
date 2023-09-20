fd50d392519239b1b8af60be47474dbd8e44c89e
ssktyOar <145244435+ssktyOar@users.noreply.github.com>
2023-09-20 17:16:55 +1200

    Add files via upload

---
+++ Code examples that I have been inspired/Unity example 1.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+// C# example.
+
+public class ExampleClass : MonoBehaviour
+{
+    // See Order of Execution for Event Functions (https://docs.unity3d.com/Manual/ExecutionOrder.html) for information on FixedUpdate() and Update() related to physics queries
+    void FixedUpdate()
+    {
+        // Bit shift the index of the layer (8) to get a bit mask
+        int layerMask = 1 << 8;
+
+        // This would cast rays only against colliders in layer 8.
+        // But instead we want to collide against everything except layer 8. The ~ operator does this, it inverts a bitmask.
+        layerMask = ~layerMask;
+
+        RaycastHit hit;
+        // Does the ray intersect any objects excluding the player layer
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
+        {
+            // If so, in debug mode draw yellow ray from center of current game object to point where it hit amothr object
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            Debug.Log("Did Hit");
+        }
+        else
+        {
+            // Otherwise in debug mode draw white ray from center of current object with length 1000
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+            Debug.Log("Did not Hit");
+        }
+    }
+}
